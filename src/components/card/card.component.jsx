e28a2c24f4da8d5cbc82e28a9b0d29b9d306e6db
Fady Soliman <fady.soliman@valocity.co.nz>
2021-07-08 17:11:16 +1200

    added a style for monsters images

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import "./card.component.css";
 export const Card = ({ monster }) => {
   return (
     <div className="card-container">
-      <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt="monster" />
+      <img className="profile-image" src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt="monster" />
       <h2>{monster.name}</h2>
       <p>{monster.email}</p>
     </div>
